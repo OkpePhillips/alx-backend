f569c41e85519a3e604959d0ccd533596e478ee5
Godwin Okpe <okpegodwin18@yahoo.com>
2024-02-19 18:06:09 +0000

    Added more functionality for translation

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -2,8 +2,8 @@
 """
 Module for the localisation
 """
-from flask import Flask, render_template
-from flask_babel import Babel
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
 
 
 app = Flask(__name__)
@@ -19,8 +19,24 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    """
+    Returns a translation according to location
+    """
+    if 'locale' in request.args:
+        requested_locale = request.args['locale']
+        if requested_locale in app.config['LANGUAGES']:
+            return requested_locale
+    return request.accept_languages.best_match(
+            app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index():
+    """
+    Returns an index page
+    """
     return render_template('index.html')
 
 
