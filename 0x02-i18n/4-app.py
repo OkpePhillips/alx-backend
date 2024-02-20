d018adfeedb38efd5904e57c15c95154070a06da
Godwin Okpe <okpegodwin18@yahoo.com>
2024-02-20 15:25:33 +0000

    Fixed documentation

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -11,6 +11,9 @@ babel = Babel(app)
 
 
 class Config:
+    """
+    Config class for Babel
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
