60ab8dd1d3e2e588e8cffe5cac19be70e506a954
Godwin Okpe <okpegodwin18@yahoo.com>
2024-03-06 21:34:30 +0000

    Attempting redis tasks in javascript

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,30 @@
+import redis from 'redis';
+import { promisify } from 'utils';
+
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const reply = await getAsync(schoolName);
+    console.log(`${reply}`);
+  } catch (err) {
+    console.error(`${err.message}`);
+    }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
---
+++ 0x03-queuing_system_in_js/README.md
@@ -0,0 +1,3 @@
+# 0x03-queuing_system_in_js
+
+Implementing a queuing system in Javascript using REDIS
